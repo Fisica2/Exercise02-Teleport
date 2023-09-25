0d7304d938a878a404b711bae3347452f1eec34f
Guillem Alqueza Mayench <guillem.alqueza@estudiantat.upc.edu>
2023-09-25 10:53:51 +0200

    game

---
+++ Game.cpp
@@ -0,0 +1,152 @@
+#include "Game.h"
+#include <math.h>
+
+Game::Game() {}
+Game::~Game(){}
+
+bool Game::Init()
+{
+	//Initialize SDL with all subsystems
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return false;
+	}
+	//Create our window: title, x, y, w, h, flags
+	Window = SDL_CreateWindow("Spaceship: arrow keys + space, f1: god mode", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	if (Window == NULL)
+	{
+		SDL_Log("Unable to create window: %s", SDL_GetError());
+		return false;
+	}
+	//Create a 2D rendering context for a window: window, device index, flags
+	Renderer = SDL_CreateRenderer(Window, -1, 0);
+	if (Renderer == NULL)
+	{
+		SDL_Log("Unable to create rendering context: %s", SDL_GetError());
+		return false;
+	}
+	//Initialize keys array
+	for (int i = 0; i < MAX_KEYS; ++i)
+		keys[i] = KEY_IDLE;
+
+	//Load images
+	if (!LoadImages())
+		return false;
+
+	//Init variables
+	//size: 104x82
+	Player.Init(20, WINDOW_HEIGHT >> 1, 100, 100, 5);
+	Box.Init(320, 500, 100, 100, 5);
+	Platform1.Init(20, 480, 300, 20, 5);
+	Platform2.Init(320, 600, 300, 20, 5);
+	idx_shot = 0;
+	int w;
+	SDL_QueryTexture(img_background, NULL, NULL, &w, NULL);
+	Scene.Init(0, 0, w, WINDOW_HEIGHT, 4);
+	god_mode = false;
+
+	return true;
+}
+bool Game::LoadImages()
+{
+	if(IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG)
+	{
+		SDL_Log("IMG_Init, failed to init required png support: %s\n", IMG_GetError());
+		return false;
+	}
+	img_player = SDL_CreateTextureFromSurface(Renderer, IMG_Load("box.png"));
+	if (img_player == NULL) {
+		SDL_Log("CreateTextureFromSurface failed: %s\n", SDL_GetError());
+		return false;
+	}
+	img_box = SDL_CreateTextureFromSurface(Renderer, IMG_Load("box.png"));
+	if (img_box == NULL) {
+		SDL_Log("CreateTextureFromSurface failed: %s\n", SDL_GetError());
+		return false;
+	}
+	img_platform = SDL_CreateTextureFromSurface(Renderer, IMG_Load("wood.png"));
+	if (img_platform == NULL) {
+		SDL_Log("CreateTextureFromSurface failed: %s\n", SDL_GetError());
+		return false;
+	}
+	return true;
+}
+void Game::Release()
+{
+	//Release images
+	SDL_DestroyTexture(img_background);
+	SDL_DestroyTexture(img_player);
+	SDL_DestroyTexture(img_shot);
+	IMG_Quit();
+	
+	//Clean up all SDL initialized subsystems
+	SDL_Quit();
+}
+bool Game::Input()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT)	return false;
+	}
+
+	SDL_PumpEvents();
+	const Uint8* keyboard = SDL_GetKeyboardState(NULL);
+	for (int i = 0; i < MAX_KEYS; ++i)
+	{
+		if (keyboard[i])
+			keys[i] = (keys[i] == KEY_IDLE) ? KEY_DOWN : KEY_REPEAT;
+		else
+			keys[i] = (keys[i] == KEY_REPEAT || keys[i] == KEY_DOWN) ? KEY_UP : KEY_IDLE;
+	}
+
+	return true;
+}
+bool Game::Update()
+{
+	//Read Input
+	if (!Input())	return true;
+
+	//Process Input
+	int fx = 0, fy = 0;
+	if (keys[SDL_SCANCODE_ESCAPE] == KEY_DOWN)	return true;
+	if (keys[SDL_SCANCODE_A] == KEY_REPEAT)	fx = -1;
+	if (keys[SDL_SCANCODE_D] == KEY_REPEAT)	fx = 1;
+
+
+	//Logic
+	if (rc.x <= 220 || ra.x < 320) isMoving = true;
+	else isMoving = false;
+
+	//Player update
+	if (isMoving) Player.Move(fx, fy);
+	else Box.Move(fx, fy);
+		
+	return false;
+}
+void Game::Draw()
+{
+
+	//Set the color used for drawing operations
+	SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255);
+	//Clear rendering target
+	SDL_RenderClear(Renderer);
+
+	//God mode uses red wireframe rectangles for physical objects
+
+	Player.GetRect(&rc.x, &rc.y, &rc.w, &rc.h);
+	Box.GetRect(&ra.x, &ra.y, &ra.w, &ra.h);
+	Platform1.GetRect(&rp.x, &rp.y, &rp.w, &rp.h);
+	Platform2.GetRect(&rp2.x, &rp2.y, &rp2.w, &rp2.h);
+
+	//Draw player
+	if (isMoving) SDL_RenderCopy(Renderer, img_player, NULL, &rc);
+	else SDL_RenderCopy(Renderer, img_box, NULL, &ra);
+	SDL_RenderCopy(Renderer, img_platform, NULL, &rp);
+	SDL_RenderCopy(Renderer, img_platform, NULL, &rp2);
+	
+	//Update screen
+	SDL_RenderPresent(Renderer);
+
+	SDL_Delay(10);	// 1000/10 = 100 fps max
+}
---
+++ Game.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "SDL/include/SDL.h"
+#include "SDL_image/include/SDL_image.h"
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+#pragma comment( lib, "SDL_image/libx86/SDL2_image.lib" )
+
+
+#include "Entity.h"
+
+#define WINDOW_WIDTH	1024
+#define WINDOW_HEIGHT	768
+#define MAX_KEYS		256
+#define MAX_SHOTS		32
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	bool Init();
+	bool LoadImages();
+	void Release();
+	
+	bool Input();
+	bool Update();
+	void Draw();
+
+private:
+	SDL_Window *Window;
+	SDL_Renderer *Renderer;
+	SDL_Texture *img_background, *img_player, *img_shot, *img_box, *img_platform;
+
+	Entity Player, Box, Platform1, Platform2, Shots[MAX_SHOTS], Scene;
+	int idx_shot;
+
+	bool god_mode;
+	SDL_Rect rc;
+	SDL_Rect ra;
+	SDL_Rect rp;
+	SDL_Rect rp2;
+
+	bool isMoving = true;
+
+	enum KEY_STATE { KEY_IDLE, KEY_DOWN, KEY_REPEAT, KEY_UP	};
+	KEY_STATE keys[MAX_KEYS]; 
+};
